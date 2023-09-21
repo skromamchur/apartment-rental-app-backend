aa749e922ca484c5a8943aee34d0425d949816b5
skromamchur <skromamchur@gmail.com>
2023-09-22 02:21:58 +0300

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(8000);
+  console.log("app listen on 8000");
 }
 bootstrap();
